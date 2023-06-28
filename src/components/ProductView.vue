4af0bda22e9fa1867d721fdba0fd03da407c8c57
Yurii Rybachok <drummer2009@ukr.net>
2023-06-28 11:33:23 +0300

    create product page & add title with img

---
+++ src/components/ProductView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="product">
+      <div class="image">
+        <img :src="productImage" alt="img">
+      </div>
+      <div class="content">
+        <h1>{{ product }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductView',
+  data() {
+    return {
+      product: 'Nike Air Force',
+      productImage: require('../assets/images/nike-red.jpg')
+    }
+  },
+}
+</script>
