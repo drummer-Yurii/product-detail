8053b3095e9310eed8def29b8ff5e8c53b4c7be5
Yurii Rybachok <drummer2009@ukr.net>
2023-06-28 14:05:12 +0300

    add event handling like click & mouseover

--- src/components/ProductView.vue
+++ src/components/ProductView.vue
@@ -1,23 +1,35 @@
 <template>
-  <div class="container">
-    <div class="product">
-      <div class="image">
-        <img :src="productImage" alt="img">
-      </div>
-      <div class="content">
-        <h1>{{ product }}</h1>
-        <div class="stockInfo">
-          <span class="green" v-if="inventory > 10">In stock</span>
-          <span class="amber" v-else-if="inventory <= 10 && inventory > 0">Only few left</span>
-          <span class="red" v-else>Out of stock</span>
+  <div>
+    <header id="header">
+      <div class="container">
+        <div class="cart">
+          <p><i class="fas fa-shopping-cart"></i> {{ cart }}</p>
         </div>
-        <ul class="features">
-          <li v-for="(feature, index) in features" :key="index">{{ feature }}</li>
-        </ul>
-        <div class="variants">
-          <span v-for="variant in variants" :key="variant.variantId">
-            {{ variant.variantColor }}
-          </span>
+      </div>
+    </header>
+    <div class="container">
+      <div class="product">
+        <div class="image">
+          <img :src="productImage" alt="img">
+        </div>
+        <div class="content">
+          <h1>{{ product }}</h1>
+          <div class="stockInfo">
+            <span class="green" v-if="inventory > 10">In stock</span>
+            <span class="amber" v-else-if="inventory <= 10 && inventory > 0">Only few left</span>
+            <span class="red" v-else>Out of stock</span>
+          </div>
+          <ul class="features">
+            <li v-for="(feature, index) in features" :key="index">{{ feature }}</li>
+          </ul>
+          <div class="variants">
+            <span v-for="variant in variants" :key="variant.variantId" @mouseover="updateImage(variant.variantImage)">
+              {{ variant.variantColor }}
+            </span>
+          </div>
+          <div class="addCart">
+            <button @click="addToCart">Add to Cart</button>
+          </div>
         </div>
       </div>
     </div>
@@ -32,21 +44,34 @@ export default {
       product: 'Nike Air Force',
       productImage: require('../assets/images/nike-red.jpg'),
       inventory: 0,
+      cart: 0,
       features: ["Durable leather", "Secure lace up", "Padded ankle collar"],
       variants: [
         {
           variantId: 1,
           variantColor: 'red',
+          variantImage: require('../assets/images/nike-red.jpg'),
         },
         {
           variantId: 2,
           variantColor: 'white',
+          variantImage: require('../assets/images/nike-white.jpg'),
         },
         {
           variantId: 3,
           variantColor: 'Black',
+          variantImage: require('../assets/images/nike-black.jpg'),
         },
       ]
+    }
+  },
+
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.productImage = variantImage;
     }
   },
 }
