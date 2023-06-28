035c6c061ed14813882db19df6b5afa1db7f64a7
Yurii Rybachok <drummer2009@ukr.net>
2023-06-28 12:00:40 +0300

    list rendering to show product details

--- src/components/ProductView.vue
+++ src/components/ProductView.vue
@@ -11,6 +11,14 @@
           <span class="amber" v-else-if="inventory <= 10 && inventory > 0">Only few left</span>
           <span class="red" v-else>Out of stock</span>
         </div>
+        <ul class="features">
+          <li v-for="(feature, index) in features" :key="index">{{ feature }}</li>
+        </ul>
+        <div class="variants">
+          <span v-for="variant in variants" :key="variant.variantId">
+            {{ variant.variantColor }}
+          </span>
+        </div>
       </div>
     </div>
   </div>
@@ -23,7 +31,22 @@ export default {
     return {
       product: 'Nike Air Force',
       productImage: require('../assets/images/nike-red.jpg'),
-      inventory: 100
+      inventory: 0,
+      features: ["Durable leather", "Secure lace up", "Padded ankle collar"],
+      variants: [
+        {
+          variantId: 1,
+          variantColor: 'red',
+        },
+        {
+          variantId: 2,
+          variantColor: 'white',
+        },
+        {
+          variantId: 3,
+          variantColor: 'Black',
+        },
+      ]
     }
   },
 }
