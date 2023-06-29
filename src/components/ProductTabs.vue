7672303cd50b59d20a5139d1ecf6632a8f0ac48f
Yurii Rybachok <drummer2009@ukr.net>
2023-06-29 18:09:40 +0300

    create tab links & data for selectedTabs

--- src/components/ProductTabs.vue
+++ src/components/ProductTabs.vue
@@ -1,11 +1,27 @@
 <template>
-    <div>
-        <h1>Product tabs</h1>
+    <div class="tabs">
+        <div class="tabLinks">
+            <span 
+                class="tab" 
+                v-for="(tab, index) in tabs" 
+                :key="index" 
+                @click="selectedTabs = tab"
+                :class="{activeTab: selectedTabs === tab}"
+            >
+                {{ tab }}
+            </span>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'ProductTabs'
+        name: 'ProductTabs',
+        data() {
+            return {
+                tabs: ['Reviews', 'Make a Review'],
+                selectedTabs: 'Reviews'
+            }
+        },
     }
 </script>
